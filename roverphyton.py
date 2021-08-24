e37daffed55b5889a5a1f1133749aa477d751a42
tomy285 <tomascosentino123@gmail.com>
2021-08-24 13:49:30 -0300

    se añadió un archivo en phyton

---
+++ roverphyton.py
@@ -0,0 +1,12 @@
+nua = 1
+acumulador = 0
+contador = 0
+promedio = 0
+
+while(nua != 0):
+	nua = float(input("Ingrese un numero")
+	acumulador += nua
+	contador = contador +1
+
+promedio = acumulador / contador
+print("El promedio es: ", promedio)
