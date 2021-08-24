bab1f60d4fd5f87c509d4c7781df0439740a3961
tomy285 <tomascosentino123@gmail.com>
2021-08-23 23:01:19 -0300

    se modificaron ambos archivos

---
+++ roverbase.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main ();{
+
+	cout << "hello world";
+	int nua, nub;
+	return 0;
+
+}
---
+++ rovermotor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main();{
+
+	int a, b;
+	cout << "ingrese un numero" << endl;
+	cin >> a;
+	cout << "ingrese otro numero" << endl;
+	cin >> b;
+	if (a == 1 && b == 2){
+		cout << "hello world";
+	}
+	else{
+		cout << "numero incorrecto" endl;
+	}
+	
+	
+	
+}
